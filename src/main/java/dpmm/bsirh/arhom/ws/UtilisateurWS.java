bc58b738b2a3b9aa8b74d82832a83864a1df7ab6
DEV-Nightbrood <florent.lebail@laposte.net>
2018-04-12 18:25:29 +0200

    - Mise a jour du Web Service Utiliateur WS pour prendre OPTIONS et corrections des entetes de POST sur /users/login - Ajout de Credentials

--- src/main/java/dpmm/bsirh/arhom/ws/UtilisateurWS.java
+++ src/main/java/dpmm/bsirh/arhom/ws/UtilisateurWS.java
@@ -2,6 +2,7 @@ package dpmm.bsirh.arhom.ws;
 
 
 import dpmm.bsirh.arhom.ARHOMDao;
+import dpmm.bsirh.buisness.entities.Credentials;
 import dpmm.bsirh.buisness.entities.JsonUser;
 import dpmm.bsirh.buisness.factory.JsonUserFactory;
 import lombok.extern.java.Log;
@@ -14,45 +15,61 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.util.List;
 
-@Path( "users")
+@Path("users")
 @Log
 @RequestScoped
-public class UtilisateurWS
-{
+public class UtilisateurWS {
 
 
-	@Inject
-	private ARHOMDao dao;
+    @Inject
+    private ARHOMDao dao;
 
-	@POST
-	@Produces(MediaType.TEXT_PLAIN )
-	@Path("/login")
-	public Response auth( @FormParam("identifiant")String identifiant, @FormParam("password") String password )
-	{
-		System.out.println("Tentative de connexion user : " + identifiant + " password : " + password);
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/login")
+    public Response auth(String json) {
+        Credentials cred = JsonbBuilder.newBuilder().build().fromJson(json, Credentials.class);
+        System.out.println("Tentative de connexion user : " + cred.getUsername() + " password : " + cred.getPassword());
 
-		return Response.ok("User"+identifiant+" Allowed").build();
+        return Response.ok(/*JsonbBuilder.create().toJson(users),MediaType.APPLICATION_JSON_TYPE*/)
+        .header("Access-Control-Allow-Origin", "*")
+        .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, Access-Control-Allow-Headers, Access-Control-Allow-Methods")
+        .header("Access-Control-Allow-Credentials", "true")
+        .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+        .header("Access-Control-Max-Age", "1209600")
+                .build();
+    }
 
-	}
+    @OPTIONS
+    @Path("/login")
+    public Response authOptions() {
+        System.out.print("OPTIONS /login");
+        return Response.ok()
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization, Access-Control-Allow-Headers, Access-Control-Allow-Methods")
+                .header("Access-Control-Allow-Credentials", "true")
+                .header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD")
+                .header("Access-Control-Max-Age", "1209600")
+                .build();
+    }
 
-	@GET
-	@Path("/all")
-	@Produces(MediaType.APPLICATION_JSON )
-	public Response listUtilisateurs()
-	{
-		List<JsonUser> users = JsonUserFactory.getUsers(dao.readAll());
+    @GET
+    @Path("/all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response listUtilisateurs() {
+        List<JsonUser> users = JsonUserFactory.getUsers(dao.readAll());
 
-		return Response.ok(JsonbBuilder.create().toJson(users),MediaType.APPLICATION_JSON_TYPE)
-				.header("Access-Control-Allow-Origin", "*")
-				.header("Access-Control-Allow-Methods", "POST, GET, PUT, UPDATE, OPTIONS")
-				.header("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With")
-				.build();
+        return Response.ok(JsonbBuilder.create().toJson(users), MediaType.APPLICATION_JSON_TYPE)
+                .header("Access-Control-Allow-Origin", "*")
+                .header("Access-Control-Allow-Methods", "*")
+                .header("Content-Type", "application/json")
+                .header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding,access-control-allow-headers")
 
-
-
+                .build();
 
 
-	}
+    }
 
 
 }
---
+++ src/main/java/dpmm/bsirh/buisness/entities/Credentials.java
@@ -0,0 +1,11 @@
+package dpmm.bsirh.buisness.entities;
+
+import lombok.Data;
+
+@Data
+public class Credentials {
+
+    private String username;
+    private String password;
+
+}
